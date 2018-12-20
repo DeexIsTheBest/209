1ef9265b1ac204fa3b097dc04e0d5e9db1547aaa
DeexIsTheBest <45711286+DeexIsTheBest@users.noreply.github.com>
2018-12-20 09:45:35 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-
-
-
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return;
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | **.# Welcome To  Moments | Server, , :black_heart::crown:**' , `اهلاً بك في السيرفر, ${member}`)
-        .addField(':		id: | user :', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                     
-                                     .addField(' -Moments', `${member.guild.name}`,true)
-                                       
-     .setFooter(`${member.guild.name}`)
-        .setTimestamp()
-   
-      channel.sendEmbed(embed);
-    });
-    
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`لقد خرج صديق :( :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`مع السلامة يحبي :raised_hand::skin-tone-1: :pensive: `)
-        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RED')
-        .setFooter(`====شكرا للاستمتاع====`, ' https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-    
-    var channel =member.guild.channels.find('name', 'welcome')
-    if (!channel) return;
-    channel.send({embed : embed});
-    })
-	
-	
-	
-	
-	
-	
-	
-	
-client.login('');
+client.on('guildMemberAdd', (member) => {
+    var channel = member.guild.channels.find('name', 'moments-📖');
+channel.send(`@${member.user.tag} تم اعطاء العضو رتبت ممبر`)
+member.addRole(member.guild.roles.find('name', '-Moments'));
+});
